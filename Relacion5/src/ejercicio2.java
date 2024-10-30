ab5ff5094216918f62b424c805174e01f653ac75
Cristina2M <cmormar2509@g.educaand.es>
2024-10-30 14:19:24 +0100

    Add files via upload

---
+++ Relacion5/src/ejercicio2.java
@@ -0,0 +1,24 @@
+public class ejercicio2 {
+    public static void main(String[] args) throws Exception {
+        byte edad;
+        short códgioPostal;
+        float altura;
+        char genero;
+        String nombre;
+        byte hijos;
+        float iva;
+        byte tallaCamisa;
+        float peso;
+        float precio;
+        boolean alumnoRepetidor;
+        String mensaje;
+        char letra;
+        byte mayorEdad;
+        byte minutos;
+        short dias;
+        int matriculaCoche;
+        long contador;
+        boolean mayorDeEdad;
+        String tallaCamiseta;
+    }
+}
---
+++ Relacion5/src/ejercicio4.java
@@ -0,0 +1,52 @@
+public class ejercicio4 {
+    public static void main(String[] args) throws Exception {
+
+        int x=2, y=3;
+        double n=1.0d, m=4.0d;
+
+        System.out.println("Las variables son: ");
+        System.out.println("X= " + x);
+        System.out.println("Y= " + y);
+        System.out.println("N= " + n);
+        System.out.println("M= " + m);
+
+
+        System.out.println("x+y=" + (x+y));
+
+
+        System.out.println("x-y= " + (x-y));
+    
+
+        System.out.println("x*y= " + (x*y));
+
+        System.out.println("x/y= " + (x/y));
+
+        System.out.println("x%y= " + (x%y));
+
+        System.out.println("n+m= " + (n+m));
+
+        System.out.println("n-m= " + (n-m));
+
+        System.out.println("n*m= " + (n*m));
+
+        System.out.println("n/m= " + (n/m));
+
+        System.out.println("n%m= " + (n%m));
+
+        System.out.println("x+n= " + (x+n));
+
+        System.out.println("y/m= " + (y/m));
+
+        System.out.println("y%m= " + (y%m));
+
+        System.out.println("Doble de X= " + (x*2));
+        System.out.println("Doble de Y= " + (y*2));
+        System.out.println("Doble de N= " + (n*2));
+        System.out.println("Doble de M= " + (m*2));
+
+        System.out.println("x+y+n+m= " + (x+y+n+m));
+
+        System.out.println("x*y*n*m= " + (x*y*n*m));
+
+    }
+}
